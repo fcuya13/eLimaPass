db8108a6825e2248f66e9d32837153638971fbc6
Franco Cuya <francocuya13@gmail.com>
2024-10-29 16:22:21 -0500

    unit testing - validators

--- lib/util/validators.dart
+++ lib/util/validators.dart
@@ -1,29 +1,34 @@
 mixin Validators {
-  String? validatePassword(String? value){
+  String? validatePassword(String? value) {
     if (value == null || value.trim().length < 8) {
       return 'Ingrese una contraseña válida';
     }
     return null;
   }
 
-  String? validateEmail(String? value){
-    if (value == null || !value.contains("@")) {
+  String? validateEmail(String? value) {
+    // Expresión regular para validar un email con dominio
+    final emailRegex = RegExp(r'^[\w.-]+@[a-zA-Z\d-]+\.[a-zA-Z]{2,}$');
+
+    if (value == null || !emailRegex.hasMatch(value.trim())) {
       return 'Ingrese un correo válido';
     }
     return null;
   }
 
-  String? validateDni(String? value){
+  String? validateDni(String? value) {
     if (value == null ||
         value.trim().length != 8 ||
-        !value.contains(RegExp(r'[0-9]'))) {
+        !value.trim().contains(RegExp(r'^[0-9]{8}'))) {
       return 'Ingrese un DNI válido';
     }
     return null;
   }
-  
-  String? validateName(String? value){
-    if (value == null || !value.contains(RegExp(r'[a-zA-Z]'))){
+
+  String? validateName(String? value) {
+    final nameRegex = RegExp(r'^[a-zA-Z\s]+$');
+
+    if (value == null || !nameRegex.hasMatch(value.trim())) {
       return "Ingrese un nombre válido";
     }
     return null;
